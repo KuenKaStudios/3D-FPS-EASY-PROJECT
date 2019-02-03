ddd9c49e9089c70281b7e0657ad89c759204e1b7
KuenKaStudios <acuencalop@gmail.com>
2019-02-03 20:03:29 +0100

    Clase 1

---
+++ Unity Project/FPS 3D/Assets/FPS_Camera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_Camera : MonoBehaviour
+{
+    public Camera FPS_mCam;
+
+    public float hSpeed;
+    public float vSpeed;
+
+    private float h;
+    private float v;
+
+    private void Awake()
+    {
+        if (FPS_mCam == null)
+        {
+            FPS_mCam = Camera.main;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        h = hSpeed * Input.GetAxis("Mouse X");
+        v = vSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0.0f, h, 0.0f);
+        FPS_mCam.transform.Rotate(v * -1, 0.0f, 0.0f);
+
+    }
+}
---
+++ Unity Project/FPS 3D/Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float mHorizontal;
+    private float mVertical;
+
+    private Rigidbody rb;
+
+    public float velocity;
+    public float jumpForce;
+
+    private Vector3 jumpV;
+
+    public bool isGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (velocity <= 0) velocity = 25f;
+        if (jumpForce <= 0) jumpForce = 2f;
+
+        jumpV = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionEnter() {
+
+        isGrounded = true;
+    }
+     
+      private void FixedUpdate(){
+        mHorizontal = Input.GetAxis("Horizontal");
+        mVertical = Input.GetAxis("Vertical");
+
+        print(" " + mVertical + ", " + mHorizontal);
+
+        Movement(mHorizontal, mVertical);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(jumpV * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void Movement(float HInput, float VInput) {
+        transform.Translate(Vector3.forward * VInput * velocity * Time.deltaTime);
+        transform.Translate(Vector3.right * HInput * velocity * Time.deltaTime);
+    }
+
+}
